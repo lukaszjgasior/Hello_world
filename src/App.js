a3c229ef826959ead5302c519b712264a2990940
lukaszjgasior <lukaszjgasior@gmail.com>
2020-09-17 11:23:56 -0400

    JS variables

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myText="Test JS";
+  console.log(myText.toUpperCase());
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {myText.toUpperCase()+1}
         </p>
         <a
           className="App-link"
