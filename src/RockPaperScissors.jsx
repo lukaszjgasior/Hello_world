d9da7fc92f1533f83c1dad8f7895e5a4ae401485
lukaszjgasior <lukaszjgasior@gmail.com>
2020-10-15 23:34:27 -0400

    RockPaperScissors app started - function practice

---
+++ src/RockPaperScissors.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function RockPaperScissors() {
+    const getUserChoice = userInput => {
+        userInput = userInput.toLowerCase();
+        if (userInput === 'rock' || userInput==='paper' || userInput==='scissors') {
+            return userInput;
+        } else {
+            console.log('Error!');
+        } 
+    }
+}
